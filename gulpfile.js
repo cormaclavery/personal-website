f6f310b294aa813ad14574f940c8cbc188d96c0f
Curry <cormac.m.lavery@gmail.com>
2016-01-05 16:14:47 +0000

    Gulp 'default' task changed to reference serve task instead of 'watch'

--- gulpfile.js
+++ gulpfile.js
@@ -4,44 +4,33 @@ var minifyCSS = require('gulp-minify-css');
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
 
-gulp.task('default', ['sass','html','watch']);
+gulp.task('default', ['sass','html','watch', 'serve']);
 
 gulp.task('serve', ['sass'], function(){
-  browserSync.init({
-    
-    server: {
-      baseDir: "build/"
-    }
-
-  });
-  gulp.watch("src/sass/*.scss", ['sass']);
-  gulp.watch("src/*.html", ['html']).on('change', browserSync.reload);
+ browserSync.init({
+   server: {
+     baseDir: "build/"
+   }
+ });
 });
 
-// gulp.task('watch', function(){
-//   gulp.watch([
-//     'src/sass/*.scss'
-//     ], ['sass']);
-  
-//   gulp.watch([
-//     'src/index.html'
-//     ], ['html']);
-// })
+gulp.task('watch', function(){
+ gulp.watch("src/sass/*.scss", ['sass']);
+ gulp.watch("src/*.html", ['html']).on('change', browserSync.reload);
+});
 
 gulp.task('html', function() {
-  return gulp.src('src/**.html')
-  .pipe(gulp.dest('build/'));
+ return gulp.src('src/**.html')
+ .pipe(gulp.dest('build/'));
 });
 
 gulp.task('sass', function() {
-  gulp.src('src/sass/*.scss')
-  .pipe(sass())
-  .pipe(minifyCSS())
-  .pipe(autoprefixer({
-    browsers: ['last 10 versions']
-  }))
-  .pipe(gulp.dest('build/styles'))
-  .pipe(browserSync.stream());
+ gulp.src('src/sass/*.scss')
+ .pipe(sass())
+ .pipe(minifyCSS())
+ .pipe(autoprefixer({
+   browsers: ['last 10 versions']
+ }))
+ .pipe(gulp.dest('build/styles'))
+ .pipe(browserSync.stream());
 });
-
-
